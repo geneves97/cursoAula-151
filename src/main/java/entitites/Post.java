7bf02595aa39da4cc871aad6e9411b415ff3573a
George <georgeneves.dev@gmail.com>
2023-10-24 10:12:27 -0300

    feat: criando o toString do post utilizando StringBuilder e criando uma variável statica sdf

--- src/main/java/entitites/Post.java
+++ src/main/java/entitites/Post.java
@@ -2,12 +2,16 @@ package entitites;
 
 import lombok.Data;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 @Data
 public class Post {
+
+    private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+
     private Date moment;
     private String title;
     private String content;
@@ -24,6 +28,21 @@ public class Post {
         this.title = title;
         this.content = content;
         this.likes = likes;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(title + "\n");
+        sb.append(likes);
+        sb.append(" Likes - ");
+        sb.append(sdf.format(moment)+ "\n");
+        sb.append(content + "\n");
+        sb.append("Comments:\n");
+        for (Comment c : comments){
+            sb.append(c.getText());
+        }
+        return sb.toString();
     }
 
     public void addComment(Comment comment){
